4db711ab3859bed1f35a4ea15bfe9fc4f3ea22ef
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-03 15:51:32 -0500

    done

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return " and ".join(items)
+    elif len(items) >= 3:
+        ret = ""
+        for el in items:
+            if items.index(el) == len(items) - 1:
+                ret += 'and ' + el
+            else:
+                ret += el + ', '
+        return ret
+    else:
+        return ", ".join(items)
